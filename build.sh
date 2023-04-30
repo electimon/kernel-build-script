775bf250d09eef74fb53ef761436a939bf07a5e1
electimon <electimon@gmail.com>
2023-04-30 12:28:54 +0000

    Add device-kernel generation

--- build.sh
+++ build.sh
@@ -19,20 +19,26 @@ function exitGracefully {
 
 # Load configuration file
 source "$scriptDir"/config
+# Export some more variables
+export DEVICE=$(basename "${DEFCONFIG}" | sed 's/_defconfig//g')
+export GEN_OUT="$PWD/android_device_$OEM_$DEVICE-kernel"
 
 # Enter the kernel directory
 cd "$scriptDir"/.. || exitGracefully
 
 # Argument handling
-while getopts "ghs" opt; do
+while getopts "gsmh" opt; do
 	case "$opt" in
 	  g) gcc=1
 		;;
 	  s) skip=1
 		;;
+	  m) gen=1
+		;;
 	  h|*) # Help
 		echo "-g for GCC compliation"
 		echo "-s for skipping defconfig copying"
+		echo "-m for making device-kernel folder"
 		echo "-h for help"
 		exit 0
 		;;
@@ -42,6 +48,15 @@ done
 shift $((OPTIND-1))
 
 [ "${1:-}" = "--" ] && shift
+
+# Clean modules
+# want fresh modules for gen'd folder
+if [ $gen ]; then
+	for i in `find out -name *.ko`; do
+		rm -rf $i
+	done
+fi
+
 # Generate defconfig
 if [ ! $skip ]; then
 	make "$DEFCONFIG" || exitGracefully
@@ -52,4 +67,26 @@ if [ ! $gcc ]; then
 	make CC=clang "$@" || exitGracefully
 else
 	make "$@" || exitGracefully
+fi
+
+# Generate device-kernel dir
+if [ $gen ]; then
+	mkdir -p $GEN_OUT/dtbs
+	mkdir -p $GEN_OUT/modules
+	if [ -f out/arch/arm64/boot/dtbo.img ]; then
+		cp out/arch/arm64/boot/dtbo.img $GEN_OUT/
+	fi
+	if [ -d out/arch/arm64/boot/dts/vendor ]; then
+		cp out/arch/arm64/boot/dts/vendor/*/*.dtb $GEN_OUT/dtbs/
+	else
+		cp out/arch/arm64/boot/dts/*/*.dtb $GEN_OUT/dtbs/
+	fi
+	for i in `find out -name *.ko`; do cp $i $GEN_OUT/modules/; done
+	if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
+		cp out/arch/arm64/boot/Image.gz-dtb $GEN_OUT/
+	elif [ -f out/arch/arm64/boot/Image.gz ]; then
+		cp out/arch/arm64/boot/Image.gz $GEN_OUT/
+	elif [ -f out/arch/arm64/boot/Image ]; then
+		cp out/arch/arm64/boot/Image $GEN_OUT/
+	fi
 fi
